d6eca0d9a60293993961f42ad8ef84a3e05d6767
swapno963 <swapno963@gmail.com>
2024-12-12 22:14:12 +0600

    first unit test of a model exists or not

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    
-    'posts.app.PostsConfig'
+
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Posts
+
+class PostModelTest(TestCase):
+    def test_post_model_exists(self):
+        posts = Posts.objects.count()
+
+        self.assertEqual(posts, 0)
