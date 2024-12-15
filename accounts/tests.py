93716994d757d2a90b49d1049ce29a627ceaa67f
swapno963 <swapno963@gmail.com>
2024-12-15 22:42:24 +0600

    form created and test and views modified

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
+from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import UserRegistrationForm
+from django.contrib.auth import get_user_model
 # Create your tests here.
 
 
 class AccountCreationTest(TestCase):
+    def setUp(self):
+        self.form_class = UserRegistrationForm
 
     def test_signup_page_exists(self):
         responce = self.client.get(reverse('signup_page'))
@@ -12,3 +17,23 @@ class AccountCreationTest(TestCase):
         self.assertEqual(responce.status_code, HTTPStatus.OK)        
         self.assertTemplateUsed('accounts/register.html')
         self.assertContains(responce, "Create Your Account Today")
+
+
+    def test_signup_form_works_properly(self):
+        self.assertTrue(issubclass(self.form_class,UserCreationForm))
+        self.assertTrue('email' in  self.form_class.Meta.fields)
+        self.assertTrue('username' in  self.form_class.Meta.fields)
+        self.assertTrue('password1' in  self.form_class.Meta.fields)
+        self.assertTrue('password2' in  self.form_class.Meta.fields)
+
+
+        sample_data ={
+            "email":"testuser@app.com",
+            "username":"testuser",
+            "password1":'p4ssword123###',
+            "password2":'p4ssword123###'
+        }
+
+        form = self.form_class(sample_data)
+
+        self.assertTrue(form.is_valid())
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .forms import UserUpdateForm
+from .forms import UserRegistrationForm
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
 # Create your views here.
 def register_user(request):
+    form = UserRegistrationForm()
+
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+
+        if form.is_valid():
+            form.save()
+
+            return redirect(reverse('login_page'))
+
+    context = {'form':form}
     return render(request,'accounts/register.html')
